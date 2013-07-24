286c3cf2ee9cbf467717b5d0a8634fcf7ed9e193
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2013-07-24 13:37:59 +0000

    Add ability to edit and display a contact

--- features/acceptance/contact.feature
+++ features/acceptance/contact.feature
@@ -8,6 +8,18 @@ Feature: Contact
     When I add a new contact
     Then I should see it on a list
 
+  Scenario: Display a contact
+    Given a contact exists
+    When I open a contact
+    Then I should details about it
+
+  Scenario: Edit a contact
+    Given a contact exists
+    When I edit a contact
+      And I open a contact
+    Then I should see new contact data
+      And I should not see old contact data
+
   Scenario: Contact listing
     Given several contacts exists
     When I open a list of contacts
--- features/step_definitions/contact_steps.rb
+++ features/step_definitions/contact_steps.rb
@@ -40,3 +40,52 @@ Then(/^I should see my contacts$/) do
     end
   end
 end
+
+
+Given(/^a contact exists$/) do
+  @contact = FactoryGirl.create(:contact)
+end
+
+When(/^I open a contact$/) do
+  visit contact_url(@contact)
+end
+
+Then(/^I should details about it$/) do
+  within ".contact" do
+    page.should have_content(@contact.first_name)
+    page.should have_content(@contact.last_name)
+    page.should have_content(@contact.email)
+    page.should have_content(@contact.phone_number)
+  end
+end
+
+When(/^I edit a contact$/) do
+  visit edit_contact_url(@contact)
+
+  within ".contact" do
+    fill_in "contact_first_name", with: "Gerald"
+    fill_in "contact_last_name", with: "Gates"
+    fill_in "contact_email", with: "gerald.gates@gmail.com"
+    fill_in "contact_phone_number", with: "30685011041"
+
+    find(".submit").click
+  end
+end
+
+Then(/^I should see new contact data$/) do
+  within ".contact" do
+    page.should have_content("Gerald")
+    page.should have_content("Gates")
+    page.should have_content("gerald.gates@gmail.com")
+    page.should have_content("30685011041")
+  end
+end
+
+Then(/^I should not see old contact data$/) do
+  within ".contact" do
+    page.should_not have_content(@contact.first_name)
+    page.should_not have_content(@contact.last_name)
+    page.should_not have_content(@contact.email)
+    page.should_not have_content(@contact.phone_number)
+  end
+end
