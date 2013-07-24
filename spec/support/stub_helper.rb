a6e4e44a49a2cb0aa283169cd855dbaa2b20c5b3
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2013-07-24 11:21:08 +0000

    Add stub_helper

---
+++ spec/support/stub_helper.rb
@@ -0,0 +1,23 @@
+module StubHelper
+
+  module_function
+
+  def stub_module(full_name, &block)
+    stub_class_or_module(full_name, Module)
+  end
+
+  def stub_class(full_name, &block)
+    stub_class_or_module(full_name, Class)
+  end
+
+  def stub_class_or_module(full_name, kind, &block)
+    full_name.to_s.split(/::/).inject(Object) do |context, name|
+      begin
+        # Give autoloading an opportunity to work
+        context.const_get(name)
+      rescue NameError
+        context.const_set(name, kind.new)
+      end
+    end
+  end
+end
